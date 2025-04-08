03acbd0ef003fb75bec8020cd95149b239e07398
SnubStumpy2007 <joshua192007@gmail.com>
2025-04-08 18:03:37 -0400

    movement
    working on player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private float PlayerSpeed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("W was pressed");
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("S was pressed");
+        } else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A was pressed");
+        } else if(Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("D was pressed");
+        }
     }
 }
