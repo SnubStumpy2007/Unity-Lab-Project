819d14179712c26d3a38e7831b16c5d4b41b93bd
SnubStumpy2007 <joshua192007@gmail.com>
2025-04-10 21:00:41 -0400

    working on player movement
    player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody PlayerRB;
+    private float PlayerSpeed = 1.0f;
 
-    private float PlayerSpeed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PlayerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,16 +18,20 @@ public class PlayerMovement : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.W))
         {
-            Debug.Log("W was pressed");
+           //Debug.Log("W was pressed");
+            PlayerRB.AddForce(Vector3.forward * PlayerSpeed, ForceMode.Impulse);
         } else if (Input.GetKeyDown(KeyCode.S))
         {
-            Debug.Log("S was pressed");
+            //Debug.Log("S was pressed");
+            PlayerRB.AddForce(Vector3.back * PlayerSpeed, ForceMode.Impulse);
         } else if (Input.GetKeyDown(KeyCode.A))
         {
-            Debug.Log("A was pressed");
+            //Debug.Log("A was pressed");
+            PlayerRB.AddForce(Vector3.left * PlayerSpeed, ForceMode.Impulse);
         } else if(Input.GetKeyDown(KeyCode.D))
         {
-            Debug.Log("D was pressed");
+            //Debug.Log("D was pressed");
+            PlayerRB.AddForce(Vector3.right * PlayerSpeed, ForceMode.Impulse);
         }
     }
 }
