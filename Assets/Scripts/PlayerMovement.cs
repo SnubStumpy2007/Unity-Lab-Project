e7619f2e6bb7a00076e32e650bc9d3dc72897c1d
SnubStumpy2007 <joshua192007@gmail.com>
2025-04-15 15:09:21 -0400

    Respawn work
    Working on respawning my player whenever it leaves the playing field.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody PlayerRB;
     private float PlayerSpeed = 1.0f;
+    private float XBoundary = 10.0f;
+    private float ZBoundary = 6.0f;
+    Vector3 OriginalPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,9 +19,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+
+        //Movement
+        if (Input.GetKeyDown(KeyCode.W))
         {
-           //Debug.Log("W was pressed");
+            //Debug.Log("W was pressed");
             PlayerRB.AddForce(Vector3.forward * PlayerSpeed, ForceMode.Impulse);
         } else if (Input.GetKeyDown(KeyCode.S))
         {
@@ -28,10 +33,31 @@ public class PlayerMovement : MonoBehaviour
         {
             //Debug.Log("A was pressed");
             PlayerRB.AddForce(Vector3.left * PlayerSpeed, ForceMode.Impulse);
-        } else if(Input.GetKeyDown(KeyCode.D))
+        } else if (Input.GetKeyDown(KeyCode.D))
         {
             //Debug.Log("D was pressed");
             PlayerRB.AddForce(Vector3.right * PlayerSpeed, ForceMode.Impulse);
         }
+
+        //Boundaries.  If a player crosses these boundaries, destroy the player, then respawn the player
+        if (PlayerRB.position.x >= XBoundary)
+        {
+            Destroy(PlayerRB);
+            Debug.Log("Player has left the play area X");
+            Respawn();
+        } else if (PlayerRB.position.z >= ZBoundary)
+        {
+            Debug.Log("Player has left the play area Z");
+        } else if (PlayerRB.position.x <= -XBoundary)
+        {
+            Debug.Log("Player has left the play area -X");
+        } else if (PlayerRB.position.z <= -ZBoundary) {
+            Debug.Log("Player has left the play area -Z");
+        } 
     }
-}
+
+        public void Respawn()
+        {
+        transform.position = OriginalPosition;
+        }
+    }
